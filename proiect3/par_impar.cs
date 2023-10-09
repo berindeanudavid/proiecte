baccf5ca56db2b1edfcee0a66abd0d175ff39b02
berindeanudavid <147379881+berindeanudavid@users.noreply.github.com>
2023-10-09 22:17:29 +0300

    Add files via upload

---
+++ proiect3/par_impar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class par_impar
+{
+    static void Main()
+    {
+        int a;
+        a = Convert.ToInt32(Console.ReadLine());
+        if(a%2==0)
+        {
+            Console.WriteLine("par");
+        }
+        else
+        {
+            Console.WriteLine("impar");
+        }
+    }
+}
