d0132702ebb4f9967f8024d4ecbd2cb44a5d3e81
berindeanudavid <147379881+berindeanudavid@users.noreply.github.com>
2023-10-09 22:28:27 +0300

    Add files via upload

---
+++ proiect4/impartire.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class impartire
+{
+    static void Main()
+    {
+        int D,i;
+        D = Convert.ToInt32(Console.ReadLine());
+        i = Convert.ToInt32(Console.ReadLine());
+        if(D%i == 0)
+        {
+            Console.WriteLine(D/i);
+        }
+        else
+        {
+            Console.WriteLine("Impartirea nu poate fi efectuata.");
+        }
+    }
+}
