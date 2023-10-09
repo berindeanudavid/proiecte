b5bdfc96592a02ec72de6aff74d0ceb62685bd06
berindeanudavid <147379881+berindeanudavid@users.noreply.github.com>
2023-10-09 21:47:05 +0300

    Add files via upload

---
+++ proiect2/Adunare.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+class Adunare
+{
+    static void Main()
+    {
+        int a,b;
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Suma lui {a} si {b} este: {a+b}");
+    }
+}
