cf1fdc5d016bdda4e1d66606af334b027228780d
berindeanudavid <147379881+berindeanudavid@users.noreply.github.com>
2023-10-09 13:14:40 +0300

    Add files via upload

---
+++ proiect/Tabelul_de_adevar_al_functiei_SI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+    Console.WriteLine("Tabel de adevar pentru operatorul AND pe biti:");
+    Console.WriteLine("x\ty\tAND");
+    Console.WriteLine("----------------------");
+    for(int x = 0;x <= 1;x++)
+        {
+        for(int y = 0; y <= 1 ; y++)
+            {
+            bool result = (x&y) == 1;
+            Console.WriteLine($"{x}\t{y}\t{result}");
+            }
+        }
+
+    }
+}
